addf597f9ca408ce6a802bcae913c6483d167297
John O'Hanley <johnohanley85@gmail.com>
2022-02-25 15:11:07 -0400

    Better instructions for centering.

--- src/planisphere/draw/transparency/DrawTransparency.java
+++ src/planisphere/draw/transparency/DrawTransparency.java
@@ -25,7 +25,6 @@ public final class DrawTransparency {
   public void draw() {
     drawProjectionBoundary();
     drawTimeScale();
-    //centeringAffordance();
     altitudes();
     meridian();
     altitudeScaleOnMeridian();
