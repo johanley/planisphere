9c572c8af1545647c60162a9dc7f829d2680657e
John O'Hanley <johnohanley85@gmail.com>
2022-03-12 12:28:53 -0400

    Old notes removed

--- src/planisphere/Build.java
+++ src/planisphere/Build.java
@@ -21,52 +21,16 @@ import planisphere.draw.transparency.GenerateTransparency;
  Build the two files for the planisphere, as a standalone program from the command line.
  
  One file is the star chart (back), and the other is a transparency (front).
-
-  --------------
+*/
+public final class Build {
   
- To-do:
-
-  Order the two-piece rivets, to be ready when I need them.
- What kind of rivet? Removable. Match the thickness!
- Some rivets allow for variable thickness.
- https://www.mcmaster.com/two-piece-rivets/
- 
-  
-  Servlet? 
-  https://kb.itextsupport.com/home/it7kb/faq/how-can-i-serve-a-pdf-to-a-browser-without-storing-a-file-on-the-server-side
-     no file, but byte arrays in memory, served to the client
-       zip together? or two separate operations, for the two files?
-     will need this project as a jar file; use it as a library. File-Export-java-jar
-  plain server, one form, two buttons for download 2 files separately, gets only, serve bytestream, error handling re-shows the form
-    year, lat, long, hours/mins, name, dec limit, format (big or small)
-    /planisphere/index.html - form; two post-targets, for two files 
-    /planisphere/build - servlet; might recycle to the form; build a config object, generate 1 file at a time
-
-  Animations: 
-    pole precession (with proper motion); proper motion in general; motion of lunar nodes
-    
-
----------------------------------------------------------------------------------------------
-
- Cloudy nights post: see https://www.cloudynights.com/topic/799573-the-planisphere/page-2, where a lady
-  is asking for a custom version.
-
- This incorrectly implies the ecliptic pole is stationary: 
- https://en.wikipedia.org/wiki/Orbital_pole#Ecliptic_pole
-
- https://www.cloudynights.com/topic/690886-planisphere-hack/
- Post to https://www.quasarastronomy.com.au/ ?
- https://www.shadowspro.com/en/index.html
- 
- Link from https://en.wikipedia.org/wiki/Planisphere
- 
+  /*
  PROBS FOUND 
    ephem.js: precession has extra line that shouldn't be there ?? 
        var temp = Math.acos(Math.sqrt(A*A + B*B)); //0..pi YES 
         ephem.δ = Math.sin(ephem.δ) * temp;  ???? NO 
    ephem.js: the Julian Date function is wrong for negative years; Math.floor should be Maths.truncate.
-*/
-public final class Build {
+ */
   
   /** 
    As a standalone program, generate the planisphere as a pair of PDF files.
